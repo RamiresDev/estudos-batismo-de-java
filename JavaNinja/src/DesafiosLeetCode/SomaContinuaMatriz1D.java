f38d4e11fb219914a612bc0398742d43d7fd3b51
RamiresDev <ramiresandre03@gmail.com>
2026-01-02 01:00:55 -0300

    feat: resolve desafio de soma acumulada (running sum)

--- JavaNinja/src/DesafiosLeetCode/SomaContinuaMatriz1D.java
+++ JavaNinja/src/DesafiosLeetCode/SomaContinuaMatriz1D.java
@@ -2,15 +2,12 @@ package DesafiosLeetCode;
 
 public class SomaContinuaMatriz1D {
         public int[] runningSum(int[] nums) {
-           // runningSum = nums [ primeiro + segundo, primeiro + segundo + terceiro e etc]
-            int[] counter = new int[]{nums.length};
 
-            for (int i = 0; i < nums.length; i++) {
-                counter[i] = nums[i];
-                for (int j = i + 1; j < nums.length; j++) {
-                    counter[i] = nums[i+j];
-                }
+            int acumulador = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                nums[i] += acumulador;
+                acumulador = nums[i];
             }
-            return counter;
+            return nums;
         }
 }
